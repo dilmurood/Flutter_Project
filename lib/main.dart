b2be8b790596726fe67eae11a142d98fd574eb5f
dilmurood <2110115@newuu.uz>
2023-12-07 17:34:04 +0500

    in prcoess

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/AddItem.dart';
 import 'package:flutter_project/AddItemShow.dart';
+import 'package:flutter_project/Cart.dart';
 import 'package:flutter_project/LoginScreen.dart';
 import 'package:flutter_project/ProfileScreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
-      home: LoginScreen(),
+      home: const Cart(),
       routes: {
         '/ProfileScreen': (context) => const ProfileScreen(),
         '/AddItemShow': (context) => const AddItemShow(),
