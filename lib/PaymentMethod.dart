b66f651f1b91beff81973173b5301ad54dface8c
dilmurood <2110115@newuu.uz>
2023-12-08 09:45:13 +0500

    cart updated

---
+++ lib/PaymentMethod.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PaymentMethod extends StatelessWidget {
+  const PaymentMethod({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
